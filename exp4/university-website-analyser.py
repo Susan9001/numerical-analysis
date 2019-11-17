6b8e3a852d7f1f5b5930536eb2726713829fd407
Susan9001 <1910602149@qq.com>
2019-11-17 10:14:03 +0800

    完成了爬及提取URL的部分

---
+++ exp4/university-website-analyser.py
@@ -0,0 +1,34 @@
+import re
+import requests
+import matplotlib as plt
+import numpy as np
+import time
+from bs4 import BeautifulSoup
+
+class UniversityWebsiteAnalyser:
+    def __init__(self, target_website="http://www.uci.edu"):
+        self.target_website = target_website
+
+    def get_all_url_on_page(self, curr_url):
+        page_soup = BeautifulSoup(requests.get(curr_url).content, 'html.parser')
+        valid_url_pattern = re.compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*(),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+        url_list = []
+        link_list = page_soup.find_all('a')
+        for link in link_list:
+            link = str(link.get('href'))
+            if re.match(valid_url_pattern, link):
+                url_list.append(link)
+        # print(url_list)
+        return url_list
+
+    def set_target_website(self, target_website):
+        self.target_website = target_website
+
+
+if __name__ == '__main__':
+    analyser = UniversityWebsiteAnalyser();
+    url_list = analyser.get_all_url_on_page("http://www.uci.edu")
+    print(url_list)
+
+
+
